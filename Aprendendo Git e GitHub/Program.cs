d5b36a2db91c8f42e8f349856bd8eb9cbe54c873
petersonstadler2 <petersonstadler@gmail.com>
2021-08-06 16:36:23 -0300

    Inicio do Projeto

---
+++ Aprendendo Git e GitHub/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aprendendo_Git_e_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
